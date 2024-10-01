fb116ffdcf2230ae78490b534851a1a1ee87f221
Lucas Ajaj Piccoli <lucas.piccoli@hcxpert.com>
2024-10-01 19:37:06 -0300

    feat: generated random ID generated is now valid brazilian CPF corresponding to module 11 algorithm criterion

--- src/main/java/com/lucashcx/app/Main.java
+++ src/main/java/com/lucashcx/app/Main.java
@@ -4,7 +4,6 @@ import com.lucashcx.app.entities.PutCandidateEIB;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         PutCandidateEIB.run();
     }
 }
--- src/main/java/com/lucashcx/app/services/WorkdayFaker.java
+++ src/main/java/com/lucashcx/app/services/WorkdayFaker.java
@@ -11,14 +11,32 @@ public class WorkdayFaker {
     private static Faker faker = new Faker(new Locale("pt-BR"));
 
     private static <T> T getRandom(T[] array) {
-    int random = new Random().nextInt(array.length);
-    return array[random];
+        int random = new Random().nextInt(array.length);
+        return array[random];
     }
 
     private static String removeSeparators(String content) {
         String toRemove = "[\\(\\)\\s\\-]";
         String parsedContent = content.replaceAll(toRemove, "");
         return parsedContent;
+    }
+
+    public static Long completeCPF(Long number) {
+        for (int i = 1; i <= 2; i++) {
+            Long copyOfNumber = number.longValue();
+            int digitMultiplier = 2;
+            int sum = 0;
+            while (copyOfNumber > 0) {
+                int digitValue = (int) (copyOfNumber % 10);
+                sum += digitValue * digitMultiplier;
+                copyOfNumber = (copyOfNumber - digitValue) / 10;
+                digitMultiplier++;
+            }
+            int mod11To11 = 11 - (sum % 11);
+            int dv = mod11To11 >= 10 ? 0 : mod11To11;
+            number = (number * 10) + dv;
+        }
+        return number;
     }
 
     public static String getFirstName() {
@@ -32,8 +50,8 @@ public class WorkdayFaker {
     public static String getPhoneNumber() {
         String rawPhoneNumber = faker.phoneNumber().phoneNumber();
         String parsedPhoneNumber = removeSeparators(rawPhoneNumber);
-        Boolean[] booleans = {false, true};
-        if(getRandom(booleans)) {
+        Boolean[] booleans = { false, true };
+        if (getRandom(booleans)) {
             parsedPhoneNumber = parsedPhoneNumber.substring(0, 2) + "9" + parsedPhoneNumber.substring(2, 10);
         }
         return parsedPhoneNumber;
@@ -66,7 +84,7 @@ public class WorkdayFaker {
     }
 
     public static String getGender() {
-        String[] genders = {"Male", "Female"};
+        String[] genders = { "Male", "Female" };
         return getRandom(genders);
     }
 
@@ -77,7 +95,8 @@ public class WorkdayFaker {
     }
 
     public static String getID() {
-        long randomNumber = faker.number().randomNumber(11, false);
-        return String.format("%011d", randomNumber);
+        Long randomNumber = faker.number().randomNumber(9, false);
+        Long cpfNumber = completeCPF(randomNumber);
+        return String.format("%011d", cpfNumber);
     }
 }
